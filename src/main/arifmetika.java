663760c21cd6b2f7847896e5f5391afae9204390
ripa <dmitriy.ripa@vistajet.com>
2022-12-07 12:59:24 +0100

    arifmetika.java addeed

---
+++ src/main/arifmetika.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class arifmetika {
+    public static void main(String[] args) {
+        int c = 5, деление), b = 7; //сложение
+        int c = a + b;
+
+        short d= (short)(c+5+a+7);
+        float a=8.5f, b=7.6f;
+        double c = a-b; //вычитание
+
+        int d = (int)a-(int)b;
+        float a = 5.8f,b=7.6f;
+
+        double c = 3*a; //умножение
+
+
+        int a = 7;
+        int b= 2;
+
+        double c =(double)a/b;
+        System.out.println(c);
+
+
+    }
+
+}
